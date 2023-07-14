c354cca1979b8e9128389d1b047ae9655da814b6
ManJurkus <129725669+ManJurkus@users.noreply.github.com>
2023-07-14 13:01:25 +0300

    finish

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 console.clear();
 
-// 1. Palyginti du skaičius a ir b. Išvesti į konsolę, kuris didesnis arba jie lygūs. (4 taškai)
+// 1. Palyginti du skaičius a ir b. Išvesti į konsolę, kuris didesnis arba jie lygūs. 
 
 console.log('--1--');
 
-const a = 5;
-const b = 8;
+const a = 2;
+const b = 2;
 
 if (a > b) {
   console.log('A didesnis uz B');
@@ -15,7 +15,7 @@ if (a > b) {
   console.log('A lygus B');
 }
 
-// 2. Naudojant for ciklą, išvesti į konsolę skaičius nuo 1 iki 10. (5 taškai)
+// 2. Naudojant for ciklą, išvesti į konsolę skaičius nuo 1 iki 10. 
 
 console.log('--2--');
 
@@ -23,7 +23,7 @@ for (let i = 1; i <= 10; i++) {
     console.log(i);
   }
 
-// 3. Naudojant for ciklą, išvesti į konsolę skaičius nuo 0, 2, 4, 6, 8, 10. (5 taškai)
+// 3. Naudojant for ciklą, išvesti į konsolę skaičius nuo 0, 2, 4, 6, 8, 10.
 
 console.log('--3--');
 
@@ -31,7 +31,7 @@ for (let i = 0; i <= 10; i += 2) {
     console.log(i);
   }
 
-// 4. Naudojant for ciklą, sugeneruoti penkis atsitiktinius skaičius nuo 1 iki 10. Išvesti juos konsolėje. (5 taškai)
+// 4. Naudojant for ciklą, sugeneruoti penkis atsitiktinius skaičius nuo 1 iki 10. Išvesti juos konsolėje.
 
 console.log('--4--');
 
@@ -40,7 +40,7 @@ for (let i = 0; i < 5; i++) {
     console.log(number);
   }
 
-// 5. Naudojant while ciklą, spausdinti atsitiktinius skaičius nuo 1 iki 10. Paskutinis atspausdintas skaičius turi būti 5. (7 taškai)
+// 5. Naudojant while ciklą, spausdinti atsitiktinius skaičius nuo 1 iki 10. Paskutinis atspausdintas skaičius turi būti 5.
 
 console.log('--5--');
 
@@ -71,12 +71,75 @@ for (let i = 0; i < masyvas.length; i++) {
   }
 }
 
-console.log('Biggest:', maxReiksme);
+console.log(masyvas, 'Biggest:', maxReiksme);
 
 // 7. Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 100. Suskaičiuokite kiek yra kiekvienos raidės. (7 taškai)
 
+console.log('--7--');
+
+const objektas = {
+  A:1,
+  B:2,
+  C:3,
+  D:4,
+}
+let minLetter = 1;
+let maxLetter = 4;
+let ilgisLetter = 100
+let masyvasLetter = [];
+
+for (let i = 0; i < 100; i++) {
+  let reiksme = Math.floor(Math.random() * (maxLetter - minLetter + 1)) + minLetter;
+  masyvasLetter.push(reiksme);
+}
+
+let daznis = {};
+for(let i=0; i<masyvasLetter.length; i++ ){
+const number = masyvasLetter[i];
+if(daznis[number]){
+  daznis[number]++;
+} else {
+daznis[number] = 1;
+  }
+} 
+
+let objektasABCD = {};
+for (let reiksme in objektas) {
+  objektasABCD[reiksme] = daznis[objektas[reiksme]];
+}
+
+
+
+console.log(masyvasLetter);
+console.log(daznis);
+console.log(objektasABCD);
+
+console.log('--8--');
+
 // 8. Parašyti funkciją - lygineSuma. Funkcijos parametrai - du kintamieji. Testų reikalavimai - abu kitamieji turi būti arba skaičiai arba masyvai(negali būti vienas skaičius, kitas masyvas).
 // Jei kintamieji skaičiai, grąžinti skaičių sumą, jei kintamieji masyvai - grąžinti masyvų ilgių sumą. Jei abu kintamieji skaičiai arba masyvai, bet suma nelyginė - grąžinti tekstą, kad suma nelyginė. (10 taškų)
+
+function lygineSuma(a,b){
+  if(typeof a === 'number' && typeof b === 'number'){
+    if ( (a+b)%2 !==0){
+      return 'Suma nelyginė'
+    }
+    return a+b
+  }
+  if(Array.isArray(a) && Array.isArray(b)){
+    return a.length+b.length
+  }
+
+  return  'Abu kitamieji turi būti arba skaičiai arba masyvai'
+
+}
+
+console.log(lygineSuma(2,4));
+console.log(lygineSuma(2,5));
+console.log(lygineSuma([2],[4]));
+console.log(lygineSuma([2],[4,5]));
+console.log(lygineSuma(2,[4]));
+console.log(lygineSuma([2],4));
 
 // 9. Parašyti funkciją pirminisSkaicius. Funkcija turi vieną kintamąjį. Turi būti patikrinimas, kad kintamasis yra skaičius. Funkcija turi grąžinti ar pateiktas skaičius yra pirminis( pirminis
 // skaičius yra tas, kuris dalinasi tik iš savęs ir tik iš vieneto be liekanos.) (10 taškų)
@@ -92,7 +155,7 @@ function pirminisSkaicius(skaicius) {
       return 'Ne';
     }
   
-    for (var i = 2; i < skaicius; i++) {
+    for (let i = 2; i < skaicius; i++) {
       if (skaicius % i === 0) {
         return 'Ne';
       }
@@ -114,6 +177,7 @@ function pirminisSkaicius(skaicius) {
 console.log('--10--');
 
 function telefonoNumeris(masyvas) {
+    let numeris = '';
     if (!Array.isArray(masyvas)) {
       return 'Ne masyvas';
     }
@@ -122,13 +186,30 @@ function telefonoNumeris(masyvas) {
       return 'Neturi';
     }
   
-    for (var i = 0; i < masyvas.length; i++) {
+    for (let i = 0; i < masyvas.length; i++) {
+      let skaicius = masyvas[i];
+
       if (typeof masyvas[i] !== 'number') {
         return 'Ne skaiciai';
       }
+      if(i === 0){
+        numeris += '(' + skaicius;
+        continue;
+      }
+      if(i === 2){
+        numeris += skaicius + ') ';
+        continue;
+      }
+      if(i === 5){
+        numeris += skaicius + '-';
+        continue;
+      }
+      numeris += skaicius;
     }
   
-    var numeris = "(" + masyvas.slice(0, 3).join("") + ") " + masyvas.slice(3, 6).join("") + "-" + masyvas.slice(6, 10).join("");
+    
+
+
   
     return numeris;
   }
